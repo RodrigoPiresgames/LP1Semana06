d5d48d7800b90e5a16f111e03f74e79b0dfda3ea
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-04-20 23:59:38 +0100

    Add Foe array creator

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,7 +6,25 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How many foes would u like?");
+            Console.WriteLine(" ");
+            string aux = Console.ReadLine();
+            int numFoes = Convert.ToInt16(aux);
+
+            Foe[] foes;
+            for (int x = 0; x < numFoes; x++)
+            {
+                Console.WriteLine("How will this one be called");
+                Console.WriteLine(" ");
+                aux = Console.ReadLine();
+
+                Foe enemy = new Foe(aux);
+
+                foes.Append(enemy);
+            }
+
+            foreach (Foe baddie in foes)
+                baddie.GetHealth;
         }
     }
 }
