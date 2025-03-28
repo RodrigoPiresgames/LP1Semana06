6fc96197ba04eedfe0aa9dcf415dc7aca66dd396
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-03-28 13:40:06 +0000

    Iniciated cronos

---
+++ Chronos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+            Thread.Sleep(500);
+
+        }
+    }
+}
